d543c6c42bb6b28e4db044048f47284118c32f75
Abhishek Shah <www.abhishekshah007@gmail.com>
2024-07-29 13:00:46 +0545

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,8 +1,11 @@
+"""Model definition and training script."""
+
 import tensorflow as tf
 from tensorflow_transform.tf_metadata import schema_utils
 from tensorflow_transform import TFTransformOutput
 
 def _build_keras_model():
+    """Builds and compiles a Keras model."""
     model = tf.keras.Sequential([
         tf.keras.layers.InputLayer(input_shape=(4,)),
         tf.keras.layers.Dense(10, activation='relu'),
@@ -12,6 +15,7 @@ def _build_keras_model():
     return model
 
 def run_fn(fn_args):
+    """Train and save the model."""
     tf_transform_output = TFTransformOutput(fn_args.transform_output)
     train_dataset = _input_fn(fn_args.train_files, tf_transform_output, batch_size=40)
     eval_dataset = _input_fn(fn_args.eval_files, tf_transform_output, batch_size=40)
@@ -21,6 +25,7 @@ def run_fn(fn_args):
     model.save(fn_args.serving_model_dir, save_format='tf')
 
 def _input_fn(file_pattern, tf_transform_output, batch_size=32):
+    """Generate dataset for training and evaluation."""
     dataset = tf.data.experimental.make_batched_features_dataset(
         file_pattern=file_pattern,
         batch_size=batch_size,
