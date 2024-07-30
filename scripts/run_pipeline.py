ee7daf421fc57347405be378c90cc88a16c9bb9d
Abhishek Shah <www.abhishekshah007@gmail.com>
2024-07-30 19:02:41 +0000

    comments updated

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,5 +1,7 @@
 """Script to run the TFX pipeline."""
 
+# tfx orchestration manages tfx containere by using the DAG - Directed Acyclic Graph
+
 from tfx.orchestration.local import LocalDagRunner
 from pipeline.pipeline import create_pipeline
 
