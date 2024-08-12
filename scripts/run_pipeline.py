f3df44d7f52ff76ac8c30f57a12bfc5de68dba06
Abhishek Shah <www.abhishekshah007@gmail.com>
2024-08-12 13:09:34 +0545

    Delete scripts directory

--- scripts/run_pipeline.py
+++
@@ -1,9 +0,0 @@
-"""Script to run the TFX pipeline."""
-
-# tfx orchestration manages tfx containere by using the DAG - Directed Acyclic Graph
-
-from tfx.orchestration.local import LocalDagRunner
-from pipeline.pipeline import create_pipeline
-
-if __name__ == '__main__':
-    LocalDagRunner().run(create_pipeline())
